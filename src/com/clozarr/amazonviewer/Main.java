f6140e23bd2736f696ab7a93526251f6b9a6117f
Carlos A Lozano A <clozanoarroyo@gmail.com>
2019-01-09 20:16:08 -0500

    Se crea el switch para seleccionar la opción deseada

--- src/com/clozarr/amazonviewer/Main.java
+++ src/com/clozarr/amazonviewer/Main.java
@@ -16,6 +16,30 @@ public class Main {
 			System.out.println("2. Series");
 			System.out.println("3. Books");
 			System.out.println("4. Magazines");
+			
+			
+			int respuesta = 0;
+			
+			switch(respuesta) {
+			  
+			   case 1:
+				   System.out.println("Movies");
+				   break;
+			   case 2:
+				   System.out.println("Series");
+				   break;
+			   case 3:
+				   System.out.println("Books");
+				   break;
+			   case 4:
+				   System.out.println("Magazines");
+				   break;
+				   
+			   default:
+			       System.out.println("Opción no válida");
+			       break;
+			}
+			
 
 		} while (exit != 0);
 
