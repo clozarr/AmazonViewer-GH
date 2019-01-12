9a952de2ba88df76dea35328da084c27d406323c
Carlos A Lozano A <clozanoarroyo@gmail.com>
2019-01-12 18:44:18 -0500

    Se crea la clase Film y se aplica herancia a Movie, Serie y chapter

--- src/com/clozarr/amazonviewer/model/Chapter.java
+++ src/com/clozarr/amazonviewer/model/Chapter.java
@@ -1,78 +1,31 @@
 package com.clozarr.amazonviewer.model;
 
-public class Chapter {
+public class Chapter extends Movie{
 
 	private int id;
-	private String tittle;
-	private int duration;
-	private short year;
-	private boolean viewed;
-	private int timeViewed;
 	private int seasonNumber;
 	
-	public Chapter(String tittle, int duration, short year) {
-		super();
-		this.tittle = tittle;
-		this.duration = duration;
-		this.year = year;
+
+
+	public Chapter(String tittle, String genre, String creator, int duration, short year, int seasonNumber) {
+		super(tittle, genre, creator, duration, year);
+		this.seasonNumber = seasonNumber;
 	}
 
-	public int getId() {
-		return id;
-	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getTittle() {
-		return tittle;
-	}
-
-	public void setTittle(String tittle) {
-		this.tittle = tittle;
-	}
-
-	public int getDuration() {
-		return duration;
-	}
-
-	public void setDuration(int duration) {
-		this.duration = duration;
-	}
-
-	public short getYear() {
-		return year;
-	}
-
-	public void setYear(short year) {
-		this.year = year;
-	}
-
-	public boolean isViewed() {
-		return viewed;
-	}
-
-	public void setViewed(boolean viewed) {
-		this.viewed = viewed;
-	}
-
-	public int getTimeViewed() {
-		return timeViewed;
-	}
-
-	public void setTimeViewed(int timeViewed) {
-		this.timeViewed = timeViewed;
-	}
 
 	public int getSeasonNumber() {
 		return seasonNumber;
 	}
 
+
 	public void setSeasonNumber(int seasonNumber) {
 		this.seasonNumber = seasonNumber;
 	}
 
+
+
 	
+
 	
 }
--- src/com/clozarr/amazonviewer/model/Movie.java
+++ src/com/clozarr/amazonviewer/model/Movie.java
@@ -1,98 +1,30 @@
 package com.clozarr.amazonviewer.model;
 
-public class Movie {
+public class Movie extends Film{
 
 	 private int id;
-	 private String tittle;
-	 private String genre;
-	 private String creator;
-	 private int duration;
-	 private short year;
-	 private boolean viewed;
 	 private int timeViewed;
 	 
-	public Movie(String tittle, String genre, short year) {
-		
-		super();
-		this.tittle = tittle;
-		this.genre = genre;
-		this.year = year;
-	}
+
 
 	public Movie(String tittle, String genre, String creator, int duration, short year) {
-		
-		super();
-		this.tittle = tittle;
-		this.genre = genre;
-		this.creator = creator;
-		this.duration = duration;
-		this.year = year;
+		super(tittle, genre, creator, duration);
+		setYear(year);
 	}
-	
-	
+
+
+
 	public void showData() {
 		
-		System.out.println("Tittle: " + this.tittle);
-		System.out.println("Genre: " + this.genre);
-		System.out.println("Year: " + this.year);
+//		System.out.println("Tittle: " + this.tittle);
+//		System.out.println("Genre: " + this.genre);
+//		System.out.println("Year: " + this.year);
 	}
 	
-	public String getTittle() {
-		
-		return this.tittle;
-	}
 	
-	public void setTittle(String tittle) {
-		
-		this.tittle = tittle;
-	}
 
 	public int getId() {
 		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getGenre() {
-		return genre;
-	}
-
-	public void setGenre(String genre) {
-		this.genre = genre;
-	}
-
-	public String getCreator() {
-		return creator;
-	}
-
-	public void setCreator(String creator) {
-		this.creator = creator;
-	}
-
-	public int getDuration() {
-		return duration;
-	}
-
-	public void setDuration(int duration) {
-		this.duration = duration;
-	}
-
-	public short getYear() {
-		return year;
-	}
-
-	public void setYear(short year) {
-		this.year = year;
-	}
-
-	public boolean isViewed() {
-		return viewed;
-	}
-
-	public void setViewed(boolean viewed) {
-		this.viewed = viewed;
 	}
 
 	public int getTimeViewed() {
